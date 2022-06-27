eaf104c5ec0e86c1cab905d2e29c4816eecbaae6
CJBuchel <connorbuchel@gmail.com>
2022-06-27 09:51:50 +0800

    Fixed up pub sub

--- Test/src/cpp/test.cpp
+++ Test/src/cpp/test.cpp
@@ -36,20 +36,25 @@ int main() {
 
   nng::set_opt_recv_timeout(client1, 100);
   nng::set_opt_recv_timeout(client2, 100);
-  nng::set_opt_recv_timeout(server, 100);
-  nng::set_opt_send_timeout(server, 100);
 
-  nng::sub::set_opt_subscribe(client1, "yes");
-  nng::sub::set_opt_subscribe(client2, "no");
+  // nng::set_opt_recv_timeout(server, 100);
+  // nng::set_opt_send_timeout(server, 100);
+
+  nng::sub::set_opt_subscribe(client1, {"DATE:", 5});
+  nng::sub::set_opt_subscribe(client2, {"TIME:", 5});
 
   client1.dial(SOCKET_BUS_ADDR);
   client2.dial(SOCKET_BUS_ADDR);
 
-  server.send("yes");
+  server.send("DATE: it's actually not a date");
+  server.send("TIME: but more");
 
   nng::buffer buffer1;
   try {
-    buffer1 = client1.recv();
+    buffer1 = client1.recv(NNG_FLAG_NONBLOCK);
+    if (buffer1.data() != NULL) {
+      std::cout << "Client 1 recv " << buffer1.data<char>() << std::endl;
+    }
   } catch(const std::exception& e) {
     // std::cerr << e.what() << '\n';
     std::cout << e.what() << std::endl;
@@ -57,19 +62,16 @@ int main() {
 
   nng::buffer buffer2;
   try {
-    buffer2 = client2.recv();
+    buffer2 = client2.recv(NNG_FLAG_NONBLOCK);
+    if (buffer2.data() != NULL) {
+      std::cout << "Client 2 recv " << buffer2.data<char>() << std::endl;
+    }
   } catch(const std::exception& e) {
     // std::cerr << e.what() << '\n';
     std::cout << e.what() << std::endl;
   }
   
-  if (buffer1.data() != NULL) {
-    std::cout << "Client 1 recv " << buffer1.data<char>() << std::endl;
-  }
 
-  if (buffer2.data() != NULL) {
-    std::cout << "Client 2 recv " << buffer2.data<char>() << std::endl;
-  }
 
   return 0;
 }
